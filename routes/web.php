bdaac41ede2309f8da98b1d532183e08d67c65dd
Stevy Endaman <stevyabessolo@gmail.com>
2022-03-30 04:34:16 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::prefix('dashboard')->group(function() {
-        Route::get('/');
+    Route::prefix('dashboard')->group(function () {
+        Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
     });
 });
